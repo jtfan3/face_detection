5462547dc661ec8d3afb8cfef2c27f824e0217a5
jtfan3 <joshuafan12340987@gmail.com>
2021-12-27 21:34:56 -0800

    added config file, minor modifications to model

---
+++ face/face_config.py
@@ -0,0 +1,15 @@
+# PATHS
+GENDER_DATA = 'D:/data/face_detection/utk/gender'
+LOG_PATH = "./logs"
+MODEL_PATH = './models/val_loss_{val_accuracy:.3f}.hdf5'
+
+# Image Variables
+IMG_SIZE = (200,200)
+COLOR_MODE = "rgb"
+IMG_SHAPE = IMG_SIZE + (3,) if COLOR_MODE == "rgb" else IMG_SIZE + (1,)
+
+# Training variables
+BATCH_SIZE = 64
+LEARNING_RATE = 0.02
+EPSILON = 0.05
+DECAY = 0.9
